94e70b7b2d6dfcf84247f17556c5da6a3565c329
lh991117 <92137419+lh991117@users.noreply.github.com>
2024-12-24 20:57:52 +0900

    14번 문제 풀이

---
+++ JavaSolution_14.java
@@ -0,0 +1,13 @@
+class JavaSolution_14 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=0; i<n; i++){
+            if(n%(i+1)==0){
+                answer+=(i+1);
+            }
+        }
+        
+        return answer;
+    }
+}
