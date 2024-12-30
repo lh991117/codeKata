e05cabfcea9eb8ccd902946df9904a07aa1f466f
lh991117 <92137419+lh991117@users.noreply.github.com>
2024-12-30 11:10:04 +0900

    문제 풀이

---
+++ JavaSolution_40.java
@@ -0,0 +1,12 @@
+class JavaSolution_40 {
+    public int solution(int n) {
+        String a = "";
+
+        while (n > 0) {
+            a = a + (n % 3);
+            n /= 3;
+        }
+
+        return Integer.parseInt(a, 3);
+    }
+}
