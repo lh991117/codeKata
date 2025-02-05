7c86a1c48d936db5316fbf8615a00e29a962f6e3
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-02-05 09:08:22 +0900

    문제해결

---
+++ JavaSolution_59.java
@@ -0,0 +1,15 @@
+class JavaSolution_59 {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int max = 0;
+
+        for (int sec : section) {
+            if (max <= sec) {
+                max = sec + m;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
